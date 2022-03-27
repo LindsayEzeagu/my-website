79e651888049b8f3b6f5f11c452ea794f1226285
UP960777 <74740661+UP960777@users.noreply.github.com>
2022-03-27 11:20:57 +0100

    fixing footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,24 +10,24 @@ const Footer = () => { return( <div className="footer">
 
 <Container>
 <Row>
-    <Col className="link" align="center"><a href="https://www.instagram.com/lindsay.tech/">
+    <Col className="link mx-auto" ><a href="https://www.instagram.com/lindsay.tech/">
     <img src={instagram} alt="INSTAGRAM"></img>
     <p>Instagram</p></a>
     </Col>
 
-    <Col className="link" align="center">
+    <Col className="link mx-auto" >
       <a href= "https://www.linkedin.com/in/lindsayezeagu/">
     <img src={linkedin} alt="LINKEDIN"></img>
 <p>Linkedin</p></a>
   
   </Col>
 
-  <Col className="link" align="center"> <a href ="https://twitter.com/LindsayEzeagu" >
+  <Col className="link align-center" > <a href ="https://twitter.com/LindsayEzeagu" >
   <img src={twitter} alt="TWITTER"></img>
   <p>Twitter</p></a>
   </Col>
 
-  <Col className="link" align="center"><a href ="https://github.com/LindsayEzeagu/">
+  <Col className="link align-center" ><a href ="https://github.com/LindsayEzeagu/">
     <img src={github} alt="GITHUB"></img>
   <p>Github</p></a>
   </Col>
